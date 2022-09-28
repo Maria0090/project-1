2f6353ec0de6400cd0030643ab5c5f5ab7a1fc28
Maria0090 <114149880+Maria0090@users.noreply.github.com>
2022-09-28 20:31:21 +0300

    Add files via upload

--- lesson 6.py
+++ lesson 6.py
@@ -13,3 +13,30 @@ for i in range(0,10):
     Numbers[i] = numbers.count(str(i))
 
 
+== {}
+b = list()
+while True:
+    t = input('Трек: ')
+    if == == 'off':
+        break
+    n = input('Имя: ')
+    if == == 'off':
+        break
+    b.append(n)
+    plase = input('Место: ')
+    if == == 'off':
+        break
+    b.append(plase)
+    a[tuple(b)] = print(a)
+
+
+
+a = {1: [3, 4], 2: [5, 6, 3], 3: [7, 1, 9, 2]}
+a[1], a[3] = a[3], a[1]
+del a[2]
+a['new_key']='new_value'
+print(a)
+
+
+
+
